7644fb60de5c5ffc3902406c9f60ebf8b1b57c0c
juyi <3323194808@qq.com>
2022-09-19 19:44:04 +0800

    位段

--- C结构体与位段/test.c
+++ C结构体与位段/test.c
@@ -82,28 +82,77 @@ struct Stu {
 //}
 
 
-#include <stdio.h>
-#pragma pack(8)  //设置默认对齐数为8
-struct S1
+//#include <stdio.h>
+//#pragma pack(8)  //设置默认对齐数为8
+//struct S1
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//#pragma pack()   //取消设置的默认对齐数，还原为默认
+//#pragma pack(1)  //设置默认对齐数为1
+//struct S2
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//#pragma pack()  //取消设置的默认对齐数，还原为默认
+//int main()
+//{
+//	//输出的结果是什么？
+//	printf("%d\n", sizeof(struct S1));
+//	printf("%d\n", sizeof(struct S2));
+//	return 0;
+//}
+//  结构在对齐方式不合适的时候，我么可以自己更改默认对齐数。
+
+//位段
+//
+//1.位段的成员必须是 int、unsigned int 或signed int 。
+//2.位段的成员名后边有一个冒号和一个数字。
+//例如：
+struct A
 {
-	char c1;
-	int i;
-	char c2;
+	int _a : 2;
+	int _b : 5;
+	int _c : 10;
+	int _d : 30;
 };
-#pragma pack()   //取消设置的默认对齐数，还原为默认
-#pragma pack(1)  //设置默认对齐数为1
-struct S2
+//A就是一个位段类型
+//int main() {
+//	struct A a;
+//	printf("%d\n", sizeof(a));  //8个字节
+//	return 0;
+//	//这里位其实是二进制位
+//}
+
+//一个例子
+struct S
 {
-	char c1;
-	int i;
-	char c2;
+	char a : 3;  //显然开辟1个字节，8bit位  
+				//当a开始用时，这8个bit位从低位开始使用,占用3个bit位
+				// 
+				// 高位		<————————————————		低位
+				//0 b b b b a a a | 0 0 0 c c c c c | 0 0 0 0 d d d d 
+	char b : 4;
+	char c : 5;
+	char d : 4;
 };
-#pragma pack()  //取消设置的默认对齐数，还原为默认
-int main()
-{
-	//输出的结果是什么？
-	printf("%d\n", sizeof(struct S1));
-	printf("%d\n", sizeof(struct S2));
+struct S s = { 0 };
+//考虑内存
+int main() {
+	//printf("%d\n", sizeof(s));  //3
+	s.a = 10;  //1010  注意a存放时只能使用3个bit位
+	s.b = 12;  //1100
+	s.c = 3;   //011
+	s.d = 4;   //100
+	//来验证存放方式
+	//0 b b b b a a a | 0 0 0 c c c c c | 0 0 0 0 d d d d
+	//0 1 1 0 0 0 1 0 | 0 0 0 0 0 0 1 1 | 0 0 0 0 0 1 0 0
+	//	6		2			0		3		0		4
+
 	return 0;
 }
-//  结构在对齐方式不合适的时候，我么可以自己更改默认对齐数。
+
