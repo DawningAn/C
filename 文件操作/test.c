7252be6bfece3748f5b5d91d7e8535b86794daff
juyi <3323194808@qq.com>
2022-10-11 20:29:27 +0800

    .

--- 文件操作/test.c
+++ 文件操作/test.c
@@ -180,21 +180,46 @@ struct Stu {
 //fscanf读取成功，返回的是读到的元素个数
 //否则不成功
 
-#include <Windows.h>
-int main() {
-	FILE* pf = fopen("test.txt", "w");
-	fputs("abcdef", pf);//先将代码放在输出缓冲区
-
-	printf("睡眠10秒-已经写数据了，打开test.txt文件，发现文件没有内容\n");
-	Sleep(10000);  //毫秒——>10s
-	printf("刷新缓冲区\n");
-	fflush(pf);//刷新缓冲区时，才将输出缓冲区的数据写到文件（磁盘）
-	//注：fflush 在高版本的VS上不能使用了
-
-	printf("再睡眠10秒-此时，再次打开test.txt文件，文件有内容了\n");
-	Sleep(10000);
-	fclose(pf);
-	//注：fclose在关闭文件的时候，也会刷新缓冲区
-	pf = NULL;
-	return 0;
+//二进制文件例子
+enum { SIZE = 5 };
+int main(void) {
+    double a[SIZE] = { 1.,2.,3.,4.,5. };
+    FILE* fp = fopen("test.bin", "wb"); // 必须用二进制模式
+    fwrite(a, sizeof * a, SIZE, fp); // 写 double 的数组
+    fclose(fp);
+    double b[SIZE];
+    fp = fopen("test.bin", "rb");
+    size_t ret_code = fread(b, sizeof * b, SIZE, fp); // 读 double 的数组
+    if (ret_code == SIZE) {
+        puts("Array read successfully, contents: ");
+        for (int n = 0; n < SIZE; ++n) printf("%f ", b[n]);
+        putchar('\n');
+    }
+    else { // error handling
+        if (feof(fp))
+            printf("Error reading test.bin: unexpected end of file\n");
+        else if (ferror(fp)) {
+            perror("Error reading test.bin");
+        }
+    }
+    fclose(fp);
 }
+
+//#include <Windows.h>
+//int main() {
+//	FILE* pf = fopen("test.txt", "w");
+//	fputs("abcdef", pf);//先将代码放在输出缓冲区
+//
+//	printf("睡眠10秒-已经写数据了，打开test.txt文件，发现文件没有内容\n");
+//	Sleep(10000);  //毫秒——>10s
+//	printf("刷新缓冲区\n");
+//	fflush(pf);//刷新缓冲区时，才将输出缓冲区的数据写到文件（磁盘）
+//	//注：fflush 在高版本的VS上不能使用了
+//
+//	printf("再睡眠10秒-此时，再次打开test.txt文件，文件有内容了\n");
+//	Sleep(10000);
+//	fclose(pf);
+//	//注：fclose在关闭文件的时候，也会刷新缓冲区
+//	pf = NULL;
+//	return 0;
+//}
